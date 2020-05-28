c41867afcafe800be1e325e13cfd3b72ac915fa7
kunluo <kunluo@umich.edu>
2020-05-28 11:54:18 +0800

    SFS

--- daiquiri/server/server.py
+++ daiquiri/server/server.py
@@ -1,10 +1,18 @@
+""" A simple file system from the server side which takes cares 
+of user/submitter's request of submitting training data or 
+downloading a trained model/predicted result
+"""
+
+import os
 import socket
 
-def fileRecv(sock):
-	filename = sock.recv(1024)
-	filesize = sock.recv(1024)
-	filesize = filesize[4:]
-	with open(filename, wb) as f:
+def fileRecv(sock, addr, name):
+	if not os.path.exist("/home/" + addr):
+		os.makedirs("/home/" + addr + "data")
+		os.makedirs("/home/" + addr + "result")
+
+	fileSize = sock.recv(1024)[4:]
+	with open(name, wb) as f:
 		data = s.recv(1024)
 		totalRecv = len(data)
 		f.write(data)
@@ -13,6 +21,18 @@ def fileRecv(sock):
 			totalRecv += len(data)
 			f.write(data)
 
+def fileRetriv(sock, addr):
+	if not os.path.isdir("/home/" + addr):
+		raise Exception("Result Not Exist")
+	sock.send("size" + str(os.path.getsize(fileName)))
+	sock.send(os.listdir)
+	with open(fileName, rb) as f:
+		dataToSend = f.read(1024)
+		sock.send(dataToSend)
+		while dataToSend != '':
+			dataToSend = f.read(1024)
+			sock.send(dataToSend)
+	sock.close()
 
 def Main():
 	host = '127.0.0.1'
@@ -24,8 +44,13 @@ def Main():
 	while True:
 		c, addr = s.accept
 		print('connected to ip {}'.format(addr))
-		fileRecv(s)
-		
+	    data = s.recv(1024)
+	    if data[:2] == "up":
+	    	fname = data[6:]
+			fileRecv(s, addr, fname)
+		elif data[:2] == "do":
+			fileRetriv(s, addr)
+
 if __name__ == '__main':
 	Main()
 		
--- daiquiri/submitter/user.py
+++ daiquiri/submitter/user.py
@@ -1,14 +1,14 @@
-""" Upload files from the client side 
+""" Simple file system, user side, upload/download files 
 """
 
 import os
 import socket
 import time
 
-def fileUpload(filePath, sock):
-	if os.path.isfile(filePath):
-		sock.send("size" + str(os.path.getsize(filePath)))
-		with open(filePath, rb) as f:
+def fileUpload(fileName, sock):
+	if os.path.isfile(fileName):
+		sock.send("size" + str(os.path.getsize(fileName)))
+		with open(fileName, rb) as f:
 			dataToSend = f.read(1024)
 			sock.send(dataToSend)
 			while dataToSend != '':
@@ -19,6 +19,20 @@ def fileUpload(filePath, sock):
 
 	sock.close()
 
+def fileRecv(sock):
+	data = sock.recv(1024)
+	fielSize = int(data[4:])
+	fileName = sock.recv(1024)
+	with open(fileName, 'wb') as f:
+		data = sock.recv(1024)
+		totalRecv = len(data)
+		f.write(data)
+		while totalRecv < fileSize:
+			data = s.recv(1024)
+			totalRecv += len(data)
+			f.write(data)
+
+
 
 def Main():
 	
@@ -26,12 +40,17 @@ def Main():
 	port = 5000
 	s = socket.socket()
 	s.connect(host, port)
-	while True:
-		if raw_input('upload a file? y/n') == 'n':
-			break
-		filename = raw_input('name of file')
-		s.send(filename)
+
+	if raw_input('upload/download/quit?') == 'upload':
+		fileName = raw_input('name of file')
+		s.send("upload" + fileName)
 		fileUpload(filename, s)
+	elif raw_input('upload/download/quit?') == 'download':
+		s.send("download")
+		fileRecv(s)
+	elif raw_input('upload/download/quit?') == 'quit':
+		break
+
 	s.close()
 		
 if __name__ == "__main__":
